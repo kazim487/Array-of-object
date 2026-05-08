dc2d37d16b009e5d90d488f7eabc0ea6e1e64a18
kazim487 <kazimraza13579@gmail.com>
2026-05-08 11:49:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var classArr = {
+name : "kazim",
+class : "12",
+rollno : "371729",
+age    : "32",
+}
+
+console.log(classArr.name)
+console.log(classArr.class)
+console.log(classArr.rollno)
+console.log(classArr.age)
+        
+    
+var classArr = {
+  name : "adil",
+  class : "mernStack",
+  rollno : "375128",
+  age    : "28", 
+}
+
+console.log(classArr.name)
+console.log(classArr.class)
+console.log(classArr.rollno)
+console.log(classArr.age)
+
+  
